1981b3183a4c5d2776cbcd9fa0ff1181b56d6af1
KBMackenzie <109933089+KBMackenzie@users.noreply.github.com>
2023-05-12 06:21:36 -0300

    Added safe file handling for all file IO

--- src/Meowscript/Parser/RunParser.hs
+++ src/Meowscript/Parser/RunParser.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Meowscript.Parser.RunParser
 ( meowParse
@@ -12,19 +13,19 @@ import Meowscript.Core.AST
 import Meowscript.Parser.Core
 import Meowscript.Parser.Expr
 import Meowscript.Parser.Statements
+import Meowscript.Utils.IO
 import qualified Data.Text as Text
-import qualified Data.Text.IO as TextIO
 import qualified Text.Megaparsec as Mega
 import Text.Megaparsec.Error (errorBundlePretty)
 import System.FilePath (takeFileName)
 
-parseFile :: (Show a) => Parser a -> FilePath -> IO (Either Text.Text a)
-parseFile parser path = do
-    let fileName = takeFileName path
-    contents <- TextIO.readFile path
-    case Mega.parse parser fileName contents of
-        (Right program) -> print program >> (return . Right) program
+parseFile :: Parser a -> FilePath -> IO (Either Text.Text a)
+parseFile parser path = safeReadFile path >>= \case
+    (Left exception) -> (return . Left) exception
+    (Right contents) -> case Mega.parse parser fileName contents of
         (Left x) -> (return . Left . Text.pack . errorBundlePretty) x
+        (Right program) -> (return . Right) program
+    where fileName = takeFileName path
 
 meowParse :: FilePath -> IO (Either Text.Text [Statement])
 meowParse = parseFile root
