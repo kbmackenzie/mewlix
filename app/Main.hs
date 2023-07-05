b311aa164ce50e0ec070f2cd889da96c5b853772
KBMackenzie <kelly.a.betty@gmail.com>
2023-07-04 23:40:19 -0300

    Cleaned up Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
---{-# LANGUAGE BangPatterns #-}
 module Main (main) where
 
-import Meowscript
 import Meowscript.REPL.Loop
-import qualified Data.Text.IO as TextIO
-import qualified Data.Map as Map
-import System.Environment (getArgs)
 
 -- To do:
 -- Make a proper CLI and REPL.
 
-main' :: IO ()
-main' = do
-    let path = "meowscript_practice6.txt"
-    --let path = "naive_fibonacci.txt"
-    --let path = "stdmeows_draft1.txt"
-    runBasic path 
-
 main :: IO ()
-main = repl -- meow "std_draft.meows" --repl
+main = repl
