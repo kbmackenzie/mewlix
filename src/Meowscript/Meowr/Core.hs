4cbde290a42c213c9f7b5adbdac97f1d0597f786
KBMackenzie <kelly.a.betty@gmail.com>
2023-07-12 13:45:27 -0300

    Added microlens dependency, about to refactor a lot of recrods

--- src/Meowscript/Meowr/Core.hs
+++ src/Meowscript/Meowr/Core.hs
@@ -1,26 +1,51 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Meowscript.Meowr.Core
-( MeowrArg(..)
+( MeowrState(..)
+, MeowrAction
+, MeowrArg(..)
+, ActionMap
+, meowrArgs
+, meowrSocket
+, meowrDefines
+, pushArgs
+, addDefine
 ) where
 
 import Meowscript.Core.AST
 import Meowscript.Utils.IO
 import qualified Data.Text as Text
 import qualified Data.Map.Strict as Map
-import qualified Text.Megaparsec as Mega
-import qualified Text.Megaparsec.Char as MChar
-import qualified Text.Megaparsec.Char.Lexer as Lexer
-import Data.Char (isSpace)
+import Network.Socket (Socket)
+import Lens.Micro.Platform
+
+data MeowrState = MeowrState --todo
+    { _meowrArgs   :: [Text.Text]
+    , _meowrSocket :: Maybe Socket
+    , _meowrDefines :: Map.Map Text.Text Text.Text }
+
+$(makeLenses ''MeowrState)
+
+type MeowrAction = MeowrArg -> MeowrState -> MeowrState
+type ActionMap = Map.Map Text.Text MeowrAction
 
 data MeowrArg =
       MeowrString Text.Text
     | MeowrFlag Text.Text
-    | MeowrOption Text.Text [Text.Text]
+    | MeowrOption Text.Text Text.Text
     deriving (Show)
 
-instance Eq MeowrArg where
-    MeowrString _   == MeowrString _   = True
-    MeowrFlag   _   == MeowrFlag   _   = True
-    MeowrOption _ _ == MeowrOption _ _ = True
-    _ == _ = False
+isFlag :: MeowrArg -> Bool 
+isFlag (MeowrFlag _) = True
+isFlag _ = False
+
+isOption :: MeowrArg -> Bool
+isOption (MeowrOption _ _) = True
+isOption _ = False
+
+pushArgs :: Text.Text -> MeowrState -> MeowrState
+pushArgs = over meowrArgs . (:)
+
+addDefine :: Text.Text -> Text.Text -> MeowrState -> MeowrState
+addDefine = (over meowrDefines .) . Map.insert
--- src/Meowscript/Meowr/Parser.hs
+++ src/Meowscript/Meowr/Parser.hs
@@ -1,15 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Meowscript.Meowr.Parser
-( 
+( parseArgs
 ) where
 
+import Meowscript.Meowr.Core
 import Meowscript.Parser.Core (Parser)
 import qualified Data.Text as Text
+import Text.Megaparsec ((<?>))
 import qualified Text.Megaparsec as Mega
 import qualified Text.Megaparsec.Char as MChar
 import qualified Text.Megaparsec.Char.Lexer as Lexer
-import Data.Char (isSpace)
+import Text.Megaparsec.Error (errorBundlePretty)
+import Control.Applicative (liftA2)
+import Data.Char (isAlphaNum)
+
+parseArgs :: Text.Text -> Either Text.Text [MeowrArg]
+parseArgs str = case Mega.parse parser "<args>" str of
+    (Left x) -> (Left . Text.pack . errorBundlePretty) x
+    (Right x) -> Right x
+    where parser = Mega.many parseArg
+
+parseArg :: Parser MeowrArg
+parseArg = Mega.choice
+    [ Mega.try parseOption          <?> "option"
+    , parseFlag                     <?> "flag"
+    , MeowrString <$> parseString   <?> "string" ]
 
 whitespace :: Parser ()
 whitespace = Lexer.space MChar.space1 Mega.empty Mega.empty 
@@ -20,16 +36,29 @@ lexeme = Lexer.lexeme whitespace
 quotes :: Parser a -> Parser a
 quotes = Mega.between (MChar.char '"') (MChar.char '"')
 
+lexChar :: Char -> Parser Char
+lexChar = lexeme . MChar.char
+
+lexChar' :: Char -> Parser (Maybe Char)
+lexChar' = Mega.optional . lexChar
+
 parseString :: Parser Text.Text
 parseString = Mega.choice
     [ parseQuoted
     , parseWord ]
 
+validChars :: [Char]
+validChars = [ '_', '.' ]
+
 parseWord :: Parser Text.Text
-parseWord = lexeme $ Mega.takeWhile1P (Just "word") (not . isSpace)
+parseWord = lexeme $ Mega.takeWhile1P (Just "word") predicate
+    where predicate = liftA2 (||) isAlphaNum (`elem` validChars)
 
 parseQuoted :: Parser Text.Text
 parseQuoted = (lexeme . quotes) $ Mega.takeWhile1P (Just "quoted") (/= '"')
 
-parseFlag :: Parser Text.Text
-parseFlag = lexeme (MChar.string "--" >> parseWord)
+parseFlag :: Parser MeowrArg
+parseFlag = MeowrFlag <$> (lexChar '-' >> parseWord)
+
+parseOption :: Parser MeowrArg
+parseOption = MeowrOption <$> (lexChar '-' >> parseWord) <*> (lexChar' '=' >> parseString)
