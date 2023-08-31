070e1797ca8b5fc45a7117318038b06cc3ee22f9
KBMackenzie <kelly.a.betty@gmail.com>
2023-08-31 03:53:58 -0300

    Added Meowscript.Parser.Run

---
+++ src/Meowscript/Parser/Run.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Meowscript.Parser.Run
+( runParser
+, parseRoot
+, parseLiftedExpr
+) where
+
+import Meowscript.Parser.AST
+import Meowscript.Parser.Utils (Parser)
+import Meowscript.Parser.Statement (root)
+import Meowscript.Parser.Expr (liftedExpr)
+import Text.Megaparsec (parse, errorBundlePretty)
+import qualified Data.Text as Text
+
+runParser :: Parser a -> FilePath -> Either Text.Text Text.Text -> Either Text.Text a
+runParser parser path contents = contents >>= \x -> case parse parser path x of
+    (Left e)  -> (Left . Text.pack . errorBundlePretty) e
+    (Right a) -> Right a
+
+parseRoot :: FilePath -> Either Text.Text Text.Text -> Either Text.Text [Statement]
+parseRoot = runParser root
+
+parseLiftedExpr :: FilePath -> Either Text.Text Text.Text -> Either Text.Text LiftedExpr
+parseLiftedExpr = runParser liftedExpr
