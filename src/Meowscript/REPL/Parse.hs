a27d43a212e8841c016e205a024e3bfb6724149f
KBMackenzie <kelly.a.betty@gmail.com>
2023-09-06 09:58:31 -0300

    Added REPL.Actions and REPL.Parse

---
+++ src/Meowscript/REPL/Parse.hs
@@ -0,0 +1,45 @@
+module Meowscript.REPL.Parse
+( parseLine
+) where
+
+import Meowscript.REPL.Actions
+import Data.Text (Text)
+import Meowscript.Parser.Expr (liftedExpr)
+import Meowscript.Parser.Utils (Parser)
+import qualified Text.Megaparsec as Mega
+import qualified Text.Megaparsec.Char as MChar
+import qualified Text.Megaparsec.Char.Lexer as Lexer
+import Control.Monad (void)
+import Data.Char (isSpace)
+
+parseLine :: Parser Line
+parseLine = Mega.choice
+    [ Meta       <$> parseCommand
+    , Expression <$> lexeme liftedExpr ]
+
+parseCommand :: Parser LineCommand
+parseCommand = do
+    (void . lexeme . MChar.char) ':'
+    command <- parseWord
+    args <- Mega.many parseString
+    return (LineCommand command args)
+
+whitespace :: Parser ()
+whitespace = Lexer.space MChar.space1 Mega.empty Mega.empty 
+
+lexeme :: Parser a -> Parser a
+lexeme = Lexer.lexeme whitespace
+
+quotes :: Parser a -> Parser a
+quotes = Mega.between (MChar.char '"') (MChar.char '"')
+
+parseString :: Parser Text
+parseString = Mega.choice
+    [ parseQuoted
+    , parseWord ]
+
+parseWord :: Parser Text
+parseWord = lexeme $ Mega.takeWhile1P (Just "word") (not . isSpace)
+
+parseQuoted :: Parser Text
+parseQuoted = (lexeme . quotes) $ Mega.takeWhile1P (Just "quoted") (/= '"')
