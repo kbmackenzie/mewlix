25ec9f70be2b5802a5a1a2c1fa708d3412e56375
KBMackenzie <kelly.a.betty@gmail.com>
2024-01-19 05:32:40 -0300

    feat: Added more Mewlix.String utils: 'brackets' and 'sepComma'

--- src/Mewlix/String/Utils.hs
+++ src/Mewlix/String/Utils.hs
@@ -4,7 +4,9 @@ module Mewlix.String.Utils
 ( (|++)
 , parens
 , quotes
+, brackets
 , surround
+, sepComma
 ) where
 
 import Data.Text (Text)
@@ -20,6 +22,12 @@ parens x = Text.concat [ "(", x, ")" ]
 quotes :: Text -> Text
 quotes x = Text.concat [ "\"", x, "\"" ]
 
+brackets :: Text -> Text
+brackets x = Text.concat [ "[", x, "]" ]
+
 surround :: Char -> Text -> Text
 surround c str = Text.concat
     [ Text.singleton c, str, Text.singleton c ]
+
+sepComma :: [Text] -> Text
+sepComma = Text.intercalate ", "
