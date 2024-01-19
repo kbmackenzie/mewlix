64c15ba4180ef99314dd4079b0a1dced89266fd3
KBMackenzie <kelly.a.betty@gmail.com>
2024-01-19 03:04:49 -0300

    feat: Changed ToJS so that it now cares about indentation

--- src/Mewlix/Compiler/ToJS.hs
+++ src/Mewlix/Compiler/ToJS.hs
@@ -15,13 +15,16 @@ import Mewlix.Utils.Show (showT)
 type IndentLevel = Int;
 
 class ToJS a where
+    transpileJS :: IndentLevel -> a -> Transpiler Text
+
     toJS :: a -> Transpiler Text
+    toJS = transpileJS 0
 
 instance ToJS Primitive where
-    toJS (MewlixInt i)      = (return . parens . showT) i
-    toJS (MewlixBool b)     = (return . parens . showT) b
-    toJS (MewlixFloat f)    = (return . parens . showT) f
-    toJS (MewlixString s)   = (return . parens . quotes . escapeString) s
-    toJS MewlixNil          = return "(null)"
-    toJS MewlixHome         = return "this"
-    toJS MewlixSuper        = return "super"
+    transpileJS _ (MewlixInt i)     = (return . parens . showT) i
+    transpileJS _ (MewlixBool b)    = (return . parens . showT) b
+    transpileJS _ (MewlixFloat f)   = (return . parens . showT) f
+    transpileJS _ (MewlixString s)  = (return . parens . quotes . escapeString) s
+    transpileJS _ MewlixNil         = return "(null)"
+    transpileJS _ MewlixHome        = return "this"
+    transpileJS _ MewlixSuper       = return "super"
