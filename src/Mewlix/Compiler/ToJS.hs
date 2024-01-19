4c99be10b34c6d8cc7d1a3102f8c8914271c6619
KBMackenzie <kelly.a.betty@gmail.com>
2024-01-19 05:07:00 -0300

    feat: Began work on transpiling Mewlix expression to Javascript

--- src/Mewlix/Compiler/ToJS.hs
+++ src/Mewlix/Compiler/ToJS.hs
@@ -8,9 +8,12 @@ import Data.Text (Text)
 import qualified Data.Text as Text
 import Mewlix.String.Escape (escapeString)
 import Mewlix.String.Utils ((|++), parens, quotes)
-import Mewlix.Compiler.Transpiler (Transpiler)
-import Mewlix.Abstract.AST (Primitive(..))
+import Mewlix.Abstract.AST
+import Mewlix.Compiler.Transpiler
 import Mewlix.Utils.Show (showT)
+import Lens.Micro.Platform ((.~), view)
+import qualified Data.HashSet as HashSet
+import qualified Data.HashMap.Strict as HashMap
 
 type IndentLevel = Int;
 
@@ -28,3 +31,113 @@ instance ToJS Primitive where
     transpileJS _ MewlixNil         = return "(null)"
     transpileJS _ MewlixHome        = return "this"
     transpileJS _ MewlixSuper       = return "super"
+
+instance ToJS Expression where
+    transpileJS _ (PrimitiveExpr prim) = toJS prim
+
+    -- Names:
+    transpileJS _ (Identifier key) = do
+        isGlobal <- asks (HashSet.member key . globalBindings)
+        if isGlobal then return key else return ("__module__." |++ key)
+
+    transpileJS _ (ObjectProperty key) = return (".box." |++ key)
+
+    -- Lists:
+    transpileJS _ (ListExpression exprs) = do
+        let makeItem :: Expression -> Transpiler Text
+            makeItem = fmap ("await " |++) . toJS
+
+        items <- mapM makeItem exprs
+        (return . Text.concat) [ "[", Text.intercalate ", " items, "]" ]
+
+{-
+data Expression =
+      PrimitiveExpr         Primitive
+    | Identifier            Key
+    | ObjectProperty        Key
+    | BooleanAnd            Expression Expression
+    | BooleanOr             Expression Expression
+    | BinaryOperation       BinaryOp Expression Expression
+    | UnaryOperation        UnaryOp Expression
+    | TernaryOperation      Expression Expression Expression
+    | ListExpression        [Expression]
+    | BoxExpression         [(Key, Expression)]
+    | Assignment            Expression Expression
+    | Increment             Expression
+    | Decrement             Expression
+    | ListPush              Expression Expression
+    | ListPop               Expression
+    | LambdaExpression      Params Expression
+    | FunctionCall          [Expression] Expression
+    | DotExpression         Expression Expression
+    | LookupExpression      Expression Expression
+    deriving (Show)
+
+data BinaryOp =
+      Addition 
+    | Subtraction
+    | Multiplication
+    | Division
+    | Modulo
+    | Power
+    | ListConcat
+    | Equal
+    | LessThan
+    | GreaterThan
+    | NotEqual
+    | GreaterOrEqual
+    | LesserOrEqual
+    deriving (Show)
+
+data UnaryOp =
+      Negation
+    | ListPeek
+    | BooleanNot
+    | LengthLookup
+    deriving (Show)
+
+newtype Block  = Block  [Statement] deriving (Show, Semigroup, Monoid)
+newtype Params = Params [Text]      deriving (Show, Semigroup, Monoid)
+
+type CatchBlock = (Maybe Expression, Block)
+
+{- A 'lifted' expression type that allows declarations.
+ - It's compiled into an IIFE if needed.                -}
+data LiftedExpression =
+      LiftExpression  Expression
+    | LiftDeclaration Key Expression
+    deriving (Show)
+
+data MewlixFunction = MewlixFunction
+    { funcName      :: Key
+    , funcParams    :: Params
+    , funcBody      :: Block       }
+    deriving (Show)
+
+data MewlixClass = MewlixClass
+    { className         :: Key
+    , classExtends      :: Maybe Key
+    , classConstructor  :: Maybe MewlixFunction
+    , classMethods      :: [MewlixFunction]      }
+    deriving (Show)
+
+data Statement =
+      ExpressionStatement   Expression
+    | WhileLoop             Expression Block
+    | ForLoop               (LiftedExpression, Expression, Expression) Block
+    | IfElse                Expression Block Block
+    | FunctionDef           MewlixFunction
+    | Declaration           Key Expression
+    | ClassDef              MewlixClass
+    | ImportStatement       FilePathT (Maybe Key)
+    | Return                Expression
+    | TryCatch              Block CatchBlock
+    | Break 
+    | Continue
+    deriving (Show)
+
+data Module = Module
+    { modulePath    :: FilePathT
+    , moduleBlock   :: Block   }
+    deriving (Show)
+ -}
--- src/Mewlix/Compiler/Transpiler.hs
+++ src/Mewlix/Compiler/Transpiler.hs
@@ -1,8 +1,14 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Mewlix.Compiler.Transpiler
-( Transpiler(..)
+( TransContext(..)
+, TransError(..)
+, Transpiler(..)
 , transpile
+-- Lens:
+, globalBindingsL
+, globalConstantsL
 -- Re-exports:
 , ask
 , asks
@@ -16,10 +22,11 @@ import Data.HashSet (HashSet)
 import Data.HashMap.Strict (HashMap)
 import Control.Monad.Reader (MonadReader, ReaderT, ask, asks, local, runReaderT)
 import Control.Monad.Except (MonadError, Except, throwError, catchError, runExcept)
+import Lens.Micro.Platform (makeLensesFor)
 
 data TransContext = TransContext
-    { ctxBindings  :: HashSet Text
-    , ctxConstants :: HashMap Text Text }
+    { globalBindings  :: HashSet Text
+    , globalConstants :: HashMap Text Text }
     deriving (Show);
 
 newtype TransError = TransError { getMessage :: Text }
@@ -36,3 +43,9 @@ newtype Transpiler a = Transpiler
 
 transpile :: TransContext -> Transpiler a -> Either TransError a
 transpile ctx = runExcept . flip runReaderT ctx . runTranspiler;
+
+{- Lens: -}
+--------------------------------------------------------------------
+$(makeLensesFor
+    [ ("globalBindings"  , "globalBindingsL"  )
+    , ("globalConstants" , "globalConstantsL" ) ] ''TransContext)
