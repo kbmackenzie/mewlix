c7f2a481b3a771adabdad39b3c26c1b8d82ea45c
KBMackenzie <kelly.a.betty@gmail.com>
2024-01-24 03:23:17 -0300

    feat: Finished writing JS transpiler for all Mewlix expressions

--- src/Mewlix/Compiler/ToJS.hs
+++ src/Mewlix/Compiler/ToJS.hs
@@ -17,7 +17,7 @@ import Mewlix.Compiler.Create
     , asFunction
     , syncCall
     , asyncCall
-    , binaryOp
+    , assignment
     , lambdaFunc
     )
 import Mewlix.Compiler.Operations (binaryOpFunc, unaryOpFunc)
@@ -48,10 +48,12 @@ instance ToJS Expression where
     transpileJS _ (PrimitiveExpr prim) = toJS prim
 
     -- Names:
+    ----------------------------------------------
     transpileJS _ (Identifier key) = return key
     transpileJS _ (ObjectProperty key) = return key
 
-    -- Lists:
+    -- Lists + boxes:
+    ----------------------------------------------
     transpileJS _ (ListExpression exprs) = do
         items <- mapM toJS exprs
         let array = (brackets . sepComma) items
@@ -68,6 +70,7 @@ instance ToJS Expression where
         wrap $ construct Mewlix.mewlixBox [ array ]
 
     -- Boolean operations:
+    ----------------------------------------------
     transpileJS _ (BooleanAnd left right) = do
         a  <- toJS left
         fb <- asFunction <$> toJS right
@@ -79,6 +82,7 @@ instance ToJS Expression where
         wrap $ syncCall (Mewlix.operation "or") [ a, fb ]
 
     -- Ternary operator:
+    ----------------------------------------------
     transpileJS _ (TernaryOperation conditionExpr left right) = do
         condition <- toJS conditionExpr
         fa <- asFunction <$> toJS left
@@ -86,17 +90,20 @@ instance ToJS Expression where
         wrap $ syncCall (Mewlix.operation "ternary") [ parens condition, fa, fb ]
 
     -- Assignment expression:
+    ----------------------------------------------
     transpileJS _ (Assignment key expr) = do
         left  <- toJS key
         right <- toJS expr
-        wrap $ binaryOp "=" left right
+        wrap $ assignment left right
 
     -- Lambda function:
+    ----------------------------------------------
     transpileJS _ (LambdaExpression params expr) = do
         body <- toJS expr
         wrap $ lambdaFunc (getParams params) body
 
     -- List expressions:
+    ----------------------------------------------
     transpileJS _ (ListPush itemExpr shelfExpr) = do
         item  <- toJS itemExpr
         shelf <- toJS shelfExpr
@@ -107,18 +114,21 @@ instance ToJS Expression where
         wrap $ syncCall (Mewlix.operation "pop") [ shelf ]
 
     -- Function calls:
+    ----------------------------------------------
     transpileJS _ (FunctionCall argExprs expr) = do
         args <- mapM toJS argExprs
         func <- toJS expr
         wrap $ asyncCall func args
 
     -- Dot expression:
+    ----------------------------------------------
     transpileJS _ (DotExpression objectExpr propertyExpr) = do
         object   <- toJS objectExpr
         property <- toJS propertyExpr
         (return . Text.concat) [ object, ".box.", property ]
 
     -- Lookup expression:
+    ----------------------------------------------
     transpileJS _ (LookupExpression objectExpr propertyExpr) = do
         let stringify = syncCall (Mewlix.mewlix "purrify") . List.singleton
         object   <- toJS objectExpr
@@ -126,132 +136,29 @@ instance ToJS Expression where
         (return . Text.concat) [ object, ".box", brackets property ]
 
     -- Binary operations:
+    ----------------------------------------------
     transpileJS _ (BinaryOperation op left right) = do
         let func = binaryOpFunc op
         args <- mapM toJS [left, right]
         wrap $ func args
 
     -- Unary operations:
+    ----------------------------------------------
     transpileJS _ (UnaryOperation op operand) = do
         let func = unaryOpFunc op
         arg  <- toJS operand
         wrap $ func [arg]
 
-    transpileJS _ _ = undefined
+    -- Increment / decrement:
+    ----------------------------------------------
+    -- Note: This assumes input has already been analyzed.
+    -- The operand should be a valid identifier.
+    transpileJS _ (Increment operand) = do
+        key <- toJS operand
+        let addOne = binaryOpFunc Addition [key, "(1)"]
+        wrap $ assignment key addOne
 
-{-
- -data BinaryOp =
-      Addition 
-    | Subtraction
-    | Multiplication
-    | Division
-    | Modulo
-    | Power
-    | ListConcat
-    | Equal
-    | LessThan
-    | GreaterThan
-    | NotEqual
-    | GreaterOrEqual
-    | LesserOrEqual
-    deriving (Show)
-
-data UnaryOp =
-      Negation
-    | ListPeek
-    | BooleanNot
-    | LengthLookup
-    deriving (Show)
-
-
- -
-data Expression =
-      PrimitiveExpr         Primitive
-    | Identifier            Key
-    | ObjectProperty        Key
-    | BooleanAnd            Expression Expression
-    | BooleanOr             Expression Expression
-    | BinaryOperation       BinaryOp Expression Expression
-    | UnaryOperation        UnaryOp Expression
-    | TernaryOperation      Expression Expression Expression
-    | ListExpression        [Expression]
-    | BoxExpression         [(Key, Expression)]
-    | Assignment            Expression Expression
-    | Increment             Expression
-    | Decrement             Expression
-    | ListPush              Expression Expression
-    | ListPop               Expression
-    | LambdaExpression      Params Expression
-    | FunctionCall          [Expression] Expression
-    | DotExpression         Expression Expression
-    | LookupExpression      Expression Expression
-    deriving (Show)
-
-data BinaryOp =
-      Addition 
-    | Subtraction
-    | Multiplication
-    | Division
-    | Modulo
-    | Power
-    | ListConcat
-    | Equal
-    | LessThan
-    | GreaterThan
-    | NotEqual
-    | GreaterOrEqual
-    | LesserOrEqual
-    deriving (Show)
-
-data UnaryOp =
-      Negation
-    | ListPeek
-    | BooleanNot
-    | LengthLookup
-    deriving (Show)
-
-newtype Block  = Block  [Statement] deriving (Show, Semigroup, Monoid)
-newtype Params = Params [Text]      deriving (Show, Semigroup, Monoid)
-
-type CatchBlock = (Maybe Expression, Block)
-
-{- A 'lifted' expression type that allows declarations.
- - It's compiled into an IIFE if needed.                -}
-data LiftedExpression =
-      LiftExpression  Expression
-    | LiftDeclaration Key Expression
-    deriving (Show)
-
-data MewlixFunction = MewlixFunction
-    { funcName      :: Key
-    , funcParams    :: Params
-    , funcBody      :: Block       }
-    deriving (Show)
-
-data MewlixClass = MewlixClass
-    { className         :: Key
-    , classExtends      :: Maybe Key
-    , classConstructor  :: Maybe MewlixFunction
-    , classMethods      :: [MewlixFunction]      }
-    deriving (Show)
-
-data Statement =
-      ExpressionStatement   Expression
-    | WhileLoop             Expression Block
-    | ForLoop               (LiftedExpression, Expression, Expression) Block
-    | IfElse                Expression Block Block
-    | FunctionDef           MewlixFunction
-    | Declaration           Key Expression
-    | ClassDef              MewlixClass
-    | ImportStatement       FilePathT (Maybe Key)
-    | Return                Expression
-    | TryCatch              Block CatchBlock
-    | Break 
-    | Continue
-    deriving (Show)
-
-data Module = Module
-    { modulePath    :: FilePathT
-    , moduleBlock   :: Block   }
-    deriving (Show)
- -}
+    transpileJS _ (Decrement operand) = do
+        key <- toJS operand
+        let takeOne = binaryOpFunc Subtraction [key, "(1)"]
+        wrap $ assignment key takeOne
