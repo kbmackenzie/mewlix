ad05357a0684545a7407544e9d8cc1088c40d82b
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-01 08:05:46 -0300

    test: Added basic util for testing parser on expressions

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
+
+import Mewlix.Parser.Run (parseExpr)
+import System.Environment (getArgs)
+import qualified Data.Text.IO as TextIO
+import Mewlix.Utils.Show (showT)
+
+textPrint :: (Show a) => a -> IO ()
+textPrint = TextIO.putStrLn . showT
+
+expression :: IO ()
+expression = do
+    path    <- head <$> getArgs
+    content <- TextIO.readFile path
+    let expr = parseExpr path content
+    textPrint expr
+
 main :: IO ()
-main = putStrLn "todo"
+main = do
+    TextIO.putStrLn "\n\n"
+    expression --putStrLn "todo"
