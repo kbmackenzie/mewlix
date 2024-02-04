209d561eb99dbe67c49ad65ffd51aa427ff13722
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-04 07:31:38 -0300

    feat: Made Indentation an opaque data type, added new function 'toIndent' to safely create a new indentation level, ensuring only positive values are valid

--- src/Mewlix/Compiler/Indentation.hs
+++ src/Mewlix/Compiler/Indentation.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Mewlix.Compiler.Indentation
-( Indentation(..)
+( Indentation
+, toIndent
 , createIndent
 , indentLine
 , indentMany
@@ -14,11 +15,22 @@ import qualified Data.List as List
 newtype Indentation = Indentation { getIndent :: Int }
     deriving (Eq, Ord, Show, Enum, Num)
 
+-- Indentation is opaque. This is the only way to make a new Indentation value.
+-- This function is intentionally a partial function.
+-- The error shouldn't ever occur unless I made a big mistake somewhere.
+toIndent :: Int -> Indentation
+toIndent n
+    | n > 0     = Indentation n
+    | otherwise = error "Mewlix.Compiler.Indentation.toIndent: Indentation cannot be a negative number!"
+
+indentSize :: Int
+indentSize = 4
+
 createIndent :: Indentation -> Text
-createIndent = Text.pack . flip List.replicate ' ' . getIndent
+createIndent = Text.pack . flip List.replicate ' ' . (* indentSize) . getIndent
 
 indentLine :: Indentation -> Text -> Text
-indentLine = flip mappend . createIndent
+indentLine = mappend . createIndent 
 
 indentMany :: Indentation -> [Text] -> [Text]
 indentMany = map . indentLine
