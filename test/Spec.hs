e25315fa255f6496a3fe6c396f0e5c86a777e472
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-04 07:35:20 -0300

    test: Added testing utils for testing compiling statements

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,6 +5,11 @@ import Mewlix.Parser.Run (parseExpr, parseRoot)
 import System.Environment (getArgs)
 import qualified Data.Text.IO as TextIO
 import Mewlix.Utils.Show (showT)
+import Control.Monad (void)
+
+import Mewlix.Abstract.AST
+import Mewlix.Compiler.Javascript.ToJS
+import Mewlix.Compiler.Javascript.Transpiler
 
 textPrint :: (Show a) => Either Text a -> IO ()
 textPrint x = case x of
@@ -18,14 +23,25 @@ expression = do
     let expr = parseExpr path content
     textPrint expr
 
-root :: IO ()
+root :: IO (Either Text YarnBall)
 root = do 
     path    <- head <$> getArgs
     content <- TextIO.readFile path
     let expr = parseRoot path content
     textPrint expr
+    return expr
+
+compileRoot :: Either Text YarnBall -> IO ()
+compileRoot (Left _) = undefined
+compileRoot (Right yarnball) = do
+    let js = toJS yarnball
+    let compilationOutput = transpile TransContext js
+    case compilationOutput of
+        (Left e) -> print e
+        (Right x) -> TextIO.putStrLn x
 
 main :: IO ()
 main = do
     TextIO.putStrLn "\n\n"
-    root --expression --putStrLn "todo"
+    x <- root --expression --putStrLn "todo"
+    compileRoot x
