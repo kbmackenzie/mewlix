9733788f452a9e2c30709ffa11ad3332c5733710
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-07 03:18:11 -0300

    feat: Added Mewlix.Project, began work on project system for Mewlix

---
+++ src/Mewlix/Project/Data.hs
@@ -0,0 +1,20 @@
+module Mewlix.Project.Data
+( ProjectMode(..)
+, ProjectData(..)
+) where
+
+import Data.Text (Text)
+import Data.HashSet (HashSet)
+
+data ProjectMode =
+      Console
+    | Graphic
+    | Library
+    deriving (Eq, Ord, Show, Read, Enum, Bounded)
+
+data ProjectData = ProjectData
+    { projectName           :: Text
+    , projectDescription    :: Text
+    , projectMode           :: ProjectMode
+    , projectDirectories    :: HashSet Text }
+    deriving (Show)
