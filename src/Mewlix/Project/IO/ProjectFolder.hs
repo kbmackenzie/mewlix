849b9ef8e78c505dd31fc9c5eeb1d01f613ed1e9
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-10 08:01:02 -0300

    feat: toOutputPath now handles absolute paths and properly replaces the file extension

--- src/Mewlix/Project/IO/ProjectFolder.hs
+++ src/Mewlix/Project/IO/ProjectFolder.hs
@@ -5,14 +5,21 @@ module Mewlix.Project.IO.ProjectFolder
 , preparePath
 ) where
 
-import System.FilePath ((</>), takeDirectory)
+import System.FilePath ((</>), takeDirectory, isAbsolute, dropDrive, replaceExtension)
 import System.Directory (getCurrentDirectory, createDirectoryIfMissing)
 
 projectFolder :: IO FilePath
 projectFolder = (</> "mewlix/user/") <$> getCurrentDirectory 
 
 toOutputPath :: FilePath -> IO FilePath
-toOutputPath = (<$> projectFolder) . flip (</>)
+toOutputPath = do
+    let handleAbsolute :: FilePath -> FilePath
+        handleAbsolute path = if isAbsolute path then dropDrive path else path
+    
+    let handleExtension :: FilePath -> FilePath
+        handleExtension = replaceExtension "js"
+
+    (<$> projectFolder) . flip (</>) . handleExtension . handleAbsolute
 
 preparePath :: FilePath -> IO ()
 preparePath = createDirectoryIfMissing True . takeDirectory
