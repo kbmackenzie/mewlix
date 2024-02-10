95be7e9e821ec746464e161f2fb0f9c745a821cd
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-10 14:25:32 -0300

    feat: Added more fields to ProjectData, made some fields optional

--- src/Mewlix/Project/Data.hs
+++ src/Mewlix/Project/Data.hs
@@ -4,36 +4,47 @@ module Mewlix.Project.Data
 ( ProjectData(..)
 ) where
 
-import Mewlix.Project.Mode (ProjectMode, readProjectMode)
-import Data.Text (Text)
+import Mewlix.Project.Mode (ProjectMode, defaultMode)
 import Data.Aeson
     ( ToJSON(..)
     , FromJSON(..)
     , withObject
     , (.:)
     , (.=)
+    , (.:?)
     , object
     )
-import qualified Data.Text as Text
-import Mewlix.Utils.Show (showT)
+import Data.Text (Text)
+import Data.HashSet (HashSet)
+import Data.HashMap.Strict (HashMap)
+import Data.Maybe (fromMaybe)
 
 data ProjectData = ProjectData
     { projectName           :: Text
     , projectDescription    :: Text
     , projectMode           :: ProjectMode
-    , projectSourceFiles    :: [FilePath]        }
+    , projectSourceFiles    :: [FilePath]
+    , projectSpecialImports :: HashMap Text Text
+    , projectFlags          :: HashSet Text      }
     deriving (Show)
 
 instance FromJSON ProjectData where
     parseJSON = withObject "ProjectData" $ \obj -> ProjectData
         <$> obj .: "name"
         <*> obj .: "description"
-        <*> fmap readProjectMode (obj .: "mode")
+        <*> optional defaultMode (obj .:? "mode")
         <*> obj .: "sources"
+        <*> optional mempty (obj .:? "specialImports")
+        <*> optional mempty (obj .:? "flags"         )
+        where
+            optional :: (Functor f) => a -> f (Maybe a) -> f a
+            optional = fmap . fromMaybe
 
 instance ToJSON ProjectData where
     toJSON project = object
-        [ "name"        .= projectName project
-        , "description" .= projectDescription project
-        , "mode"        .= (Text.toLower . showT . projectMode) project
-        , "sources"     .= projectSourceFiles project ]
+        [ "name"            .= projectName project
+        , "description"     .= projectDescription project
+        , "mode"            .= projectMode project
+        , "sources"         .= projectSourceFiles project
+        , "specialImports"  .= projectSpecialImports project
+        , "flags"           .= projectFlags project ]
