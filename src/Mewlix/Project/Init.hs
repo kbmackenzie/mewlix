c2a6550709ed02626335a89d3dd51cd2dc7d9602
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-10 12:58:01 -0300

    feat: Added Mewlix.Project.Init, with functions for reading project data

---
+++ src/Mewlix/Project/Init.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Mewlix.Project.Init
+( readProject
+) where
+
+import Conduit
+    ( runConduitRes
+    , (.|)
+    , sourceDirectory
+    , filterC
+    , headC
+    )
+import Mewlix.Project.Make
+    ( ProjectMaker
+    , liftIO
+    , throwError
+    )
+import Mewlix.Utils.Yaml (readYaml)
+import Mewlix.Project.Data (ProjectData(..))
+import System.FilePath (isExtensionOf)
+
+findProject :: ProjectMaker FilePath
+findProject = do
+    let search :: IO (Maybe FilePath) 
+        search = runConduitRes
+             $ sourceDirectory "."
+            .| filterC (isExtensionOf "mewlix")
+            .| headC
+
+    liftIO search >>= \case
+        (Just path) -> return path
+        Nothing     -> throwError "Couldn't find a '.mewlix' project file the in current directory!"
+
+readProject :: ProjectMaker ProjectData
+readProject = do
+    path <- findProject
+    readYaml path >>= \case
+        (Left err)  -> throwError $ concat
+            [ "Couldn't parse project file: \"", path, "\":",  show err ]
+        (Right dat) -> return dat
