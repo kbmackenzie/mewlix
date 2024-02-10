367e506c0a2e1a52ff0327a43ff82c79cf7e857c
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-10 14:34:37 -0300

    feat: Added readDataFile util to Mewlix.Utils.IO

--- src/Mewlix/Utils/IO.hs
+++ src/Mewlix/Utils/IO.hs
@@ -1,6 +1,7 @@
 module Mewlix.Utils.IO
 ( readFile
 , writeFile
+, readDataFile
 ) where
 
 -- This module should always be imported qualified!
@@ -11,9 +12,14 @@ import qualified Data.ByteString as ByteString
 import qualified Data.Text.Encoding as Encoding
 import Data.Text.Encoding.Error (UnicodeException)
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad ((>=>))
+import Paths_Mewlix (getDataFileName)
 
 readFile :: (MonadIO m) => FilePath -> m (Either UnicodeException Text)
 readFile = liftIO . fmap Encoding.decodeUtf8' . ByteString.readFile
 
 writeFile :: (MonadIO m) => FilePath -> Text -> m ()
 writeFile path = liftIO . ByteString.writeFile path . Encoding.encodeUtf8
+
+readDataFile :: (MonadIO m) => FilePath -> m (Either UnicodeException Text)
+readDataFile = liftIO . getDataFileName >=> readFile
