3c302a5392fc2c60589da674c4a87e9a09fa1971
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-10 23:59:54 -0300

    feat: Added prettyYaml for pretty-printing YAML with fields in the correct order

--- src/Mewlix/Utils/Yaml.hs
+++ src/Mewlix/Utils/Yaml.hs
@@ -2,11 +2,18 @@
 
 module Mewlix.Utils.Yaml
 ( readYaml
+, prettyYaml
 ) where
 
+import Data.Text (Text)
+import Data.ByteString (ByteString)
 import qualified Data.ByteString as ByteString
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Data.Yaml (decodeEither', FromJSON, ParseException)
+import Data.Yaml (decodeEither', FromJSON, ToJSON, ParseException)
+import Data.Yaml.Pretty (encodePretty, setConfCompare, defConfig)
 
 readYaml :: (MonadIO m, FromJSON a) => FilePath -> m (Either ParseException a)
 readYaml = fmap decodeEither' . liftIO . ByteString.readFile
+
+prettyYaml :: (ToJSON a) => (Text -> Text -> Ordering) -> a -> ByteString
+prettyYaml = encodePretty . flip setConfCompare defConfig
