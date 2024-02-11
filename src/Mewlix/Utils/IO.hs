129203a701dd93762b86fa2b1797c13e0b0b095b
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-10 21:54:49 -0300

    feat: Added more to Mewlix.Utils.IO; added Mewlix.Utils.Logging

--- src/Mewlix/Utils/IO.hs
+++ src/Mewlix/Utils/IO.hs
@@ -2,6 +2,7 @@ module Mewlix.Utils.IO
 ( readFile
 , writeFile
 , readDataFile
+, writeFileS
 ) where
 
 -- This module should always be imported qualified!
@@ -9,17 +10,21 @@ module Mewlix.Utils.IO
 import Prelude hiding (readFile, writeFile)
 import Data.Text (Text)
 import qualified Data.ByteString as ByteString
-import qualified Data.Text.Encoding as Encoding
+import qualified Data.ByteString.UTF8 as ByteUTF8
+import qualified Data.Text.Encoding as ByteEncoding
 import Data.Text.Encoding.Error (UnicodeException)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad ((>=>))
 import Paths_Mewlix (getDataFileName)
 
 readFile :: (MonadIO m) => FilePath -> m (Either UnicodeException Text)
-readFile = liftIO . fmap Encoding.decodeUtf8' . ByteString.readFile
+readFile = liftIO . fmap ByteEncoding.decodeUtf8' . ByteString.readFile
 
 writeFile :: (MonadIO m) => FilePath -> Text -> m ()
-writeFile path = liftIO . ByteString.writeFile path . Encoding.encodeUtf8
+writeFile path = liftIO . ByteString.writeFile path . ByteEncoding.encodeUtf8
 
 readDataFile :: (MonadIO m) => FilePath -> m (Either UnicodeException Text)
 readDataFile = liftIO . getDataFileName >=> readFile
+
+writeFileS :: (MonadIO m) => FilePath -> String -> m ()
+writeFileS path = liftIO . ByteString.writeFile path . ByteUTF8.fromString
