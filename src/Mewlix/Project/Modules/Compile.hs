74990e22f5fca702f7c58bd2e04bc2575c544012
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-12 09:01:55 -0300

    feat: Rewrote createContext project util to add mode-specific special imports to transpiler context

--- src/Mewlix/Project/Modules/Compile.hs
+++ src/Mewlix/Project/Modules/Compile.hs
@@ -1,19 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Mewlix.Project.Modules.Compile
 ( compileModules
 ) where
 
 import Mewlix.Compiler (TranspilerContext(..))
 import Mewlix.Project.Maker (ProjectMaker)
-import Mewlix.Project.Data.Types (ProjectData(..))
+import Mewlix.Project.Data.Types (ProjectData(..), ProjectMode(..))
 import Mewlix.Project.Modules.ModuleWriter (writeModules)
 import Mewlix.Project.Modules.FileSearch (processSources, validateSources)
 import Mewlix.Abstract.Key (Key(..))
 import Data.HashMap.Strict (mapKeys)
+import qualified Data.HashMap.Strict as HashMap
 
 createContext :: ProjectData -> TranspilerContext
-createContext projectData = TranspilerContext 
-    { specialImports  = (mapKeys Key . projectSpecialImports) projectData
-    , transpilerFlags = projectFlags projectData                          }
+createContext projectData = do
+    let addModeImports = case projectMode projectData of
+            Console -> HashMap.insert (Key "std.console" ) "Mewlix.MewlixConsole"
+            Graphic -> HashMap.insert (Key "std.graphics") "Mewlix.MewlixGraphics"
+            Library -> id
+
+    TranspilerContext
+        { specialImports  = (addModeImports . mapKeys Key . projectSpecialImports) projectData
+        , transpilerFlags = projectFlags projectData                                            }
 
 compileModules :: ProjectData -> ProjectMaker [FilePath]
 compileModules projectData = do
