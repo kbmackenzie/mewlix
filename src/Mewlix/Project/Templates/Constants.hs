fe5ca51fc154156c9444c474944dfe92be32ac70
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-12 07:14:36 -0300

    feat: Added template constants

--- src/Mewlix/Project/Templates/Constants.hs
+++ src/Mewlix/Project/Templates/Constants.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Mewlix.Project.Templates.Constants
-(
+( template
 ) where
+
+import Mewlix.Project.Maker (Language(..))
+import Mewlix.Project.Data.Types (ProjectMode(..))
+import System.FilePath ((</>))
 
 newtype Template = Template { getTemplate :: FilePath }
     deriving (Eq, Show)
 
---templateJS :: Language -> Template
+templatePath :: FilePath -> FilePath
+templatePath = ("templates" </>)
 
+template :: Language -> ProjectMode -> Template
+template Javascript = \case
+    Console -> Template { getTemplate = templatePath "js/console" }
+    Graphic -> Template { getTemplate = templatePath "js/graphic" }
+    Library -> Template { getTemplate = templatePath "js/library" }
