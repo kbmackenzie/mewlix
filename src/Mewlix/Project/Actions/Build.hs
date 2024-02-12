5d34a20a3bd4bf5b6e8013ab99b51706f5b391e4
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-12 18:47:05 -0300

    feat: script-list is now outputted as a JSON file

--- src/Mewlix/Project/Actions/Build.hs
+++ src/Mewlix/Project/Actions/Build.hs
@@ -1,13 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Mewlix.Project.Actions.Build
 ( buildProject
 ) where
 
-import Mewlix.Project.Maker (ProjectMaker, liftIO)
+import Mewlix.Project.Maker (ProjectMaker)
+import Mewlix.Project.Folder (coreFolder)
 import Mewlix.Project.Data.Types (ProjectData(..))
 import Mewlix.Project.Modules.Compile (compileModules)
 import Mewlix.Project.Templates.Create (createFromTemplate)
-import qualified Data.List as List
-import Mewlix.Utils.FileIO (writeFileS)
+import System.FilePath ((</>))
+import Mewlix.Utils.FileIO (writeFileBL)
+import Data.Aeson (object, (.=), encode)
 
 buildProject :: ProjectData -> ProjectMaker ()
 buildProject projectData = do
@@ -16,10 +20,13 @@ buildProject projectData = do
 
     -- Modules:
     compiledModules <- compileModules projectData
-    scriptList compiledModules
+    scriptList projectData compiledModules
 
-scriptList :: [FilePath] -> ProjectMaker ()
-scriptList paths = do
-    let targetPath = "output/mewlix/core/script-list"
-    let scripts = List.intercalate "\n" paths
-    liftIO $ writeFileS targetPath scripts
+scriptList :: ProjectData -> [FilePath] -> ProjectMaker ()
+scriptList projectData paths = do
+    targetPath <- (</> "script-list") <$> coreFolder
+    let entrypoint = "" :: String
+    let script = object
+            [ "entrypoint" .= entrypoint
+            , "scripts"    .= paths         ]
+    writeFileBL targetPath (encode script)
