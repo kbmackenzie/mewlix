dbd4bb8179c03dc39261cc8bec025dd2d3d9b37b
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-13 09:12:28 -0300

    feat: Added runProject for running build output in a local server

---
+++ src/Mewlix/Project/Actions/Server.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Mewlix.Project.Actions.Server
+( runProject
+) where
+
+import Mewlix.Project.Maker (ProjectMaker)
+import Mewlix.Project.Folder (outputFolder)
+import Mewlix.Project.Data.Types (ProjectData(..))
+import Mewlix.Project.Actions.Build (buildProject)
+import Mewlix.Project.Log (projectLog)
+import Web.Scotty (scotty, middleware, get, file)
+import Network.Wai.Handler.Warp (Port)
+import Network.Wai.Middleware.Static (staticPolicy, addBase)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad (unless)
+import System.Directory (doesDirectoryExist)
+import Mewlix.Utils.Show (showT)
+
+runServer :: (MonadIO m) => Port -> m ()
+runServer port = liftIO . scotty port $ do
+    let policy = staticPolicy (addBase outputFolder)
+    middleware policy
+    get "/" $ file "index.html"
+
+runProject :: ProjectData -> ProjectMaker ()
+runProject projectData = do
+    exists <- liftIO (doesDirectoryExist outputFolder)
+    unless exists $
+        buildProject projectData
+
+    let port = 8000 :: Port
+    projectLog projectData $ mconcat
+        ["Running project ", projectName projectData, " in port ", showT port]
+    runServer port
