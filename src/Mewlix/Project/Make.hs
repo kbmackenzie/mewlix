6e98522af343e1bb4a022235df325fdd253e620e
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-13 10:57:14 -0300

    feat: A complete rewrite of the Mewlix.Project.Make module

--- src/Mewlix/Project/Make.hs
+++ src/Mewlix/Project/Make.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE LambdaCase #-}
 
 module Mewlix.Project.Make
-( make
-, singletonProject
+( Action(..)
+, make
+, makeSingle
+-- Re-exports:
+, Language(..)
 ) where
 
 import Mewlix.Project.Maker
@@ -10,23 +13,56 @@ import Mewlix.Project.Maker
     , Language(..)
     , projectMakeJS
     )
-import Mewlix.Project.Data.Types (ProjectData(..), projectSourceFilesL)
+import Mewlix.Project.Data.Types
+    ( ProjectData(..)
+    , projectDataEmpty
+    )
+-- Actions:
 import Mewlix.Project.Actions.Build (buildProject)
+import Mewlix.Project.Actions.Clean (cleanProject)
+import Mewlix.Project.Actions.Package (packageProject)
+import Mewlix.Project.Actions.Server (runProject)
+-- Assorted:
 import Mewlix.Project.Data.Read (readProject)
 import qualified Mewlix.Utils.Logging as Logging
 import qualified Data.Text as Text
-import Lens.Micro.Platform (over)
 
-type ProjectCallback = ProjectMaker () -> IO (Either String ())
+data Action =
+      Build
+    | Clean
+    | Package
+    | Run
+    deriving (Eq, Ord, Show, Enum, Bounded)
 
-makeRun :: ProjectCallback -> ProjectMaker () -> IO ()
-makeRun callback action = callback action >>= \case
-    (Left err) -> Logging.writeStderr (Text.pack err)
-    (Right _ ) -> return ()
+type ActionFunc  = ProjectData -> ProjectMaker ()
+type ProjectFunc = ProjectMaker () -> IO (Either String ())
 
-make :: Language -> IO ()
-make Javascript = makeRun projectMakeJS (readProject >>= buildProject)
+language :: Language -> ProjectFunc
+language Javascript = projectMakeJS
 
-singletonProject :: Language -> [FilePath] -> ProjectData -> IO ()
-singletonProject Javascript files = makeRun projectMakeJS . buildProject . addFiles
-    where addFiles = over projectSourceFilesL (files ++)
+action :: Action -> ActionFunc
+action Build    = buildProject
+action Clean    = cleanProject
+action Package  = packageProject
+action Run      = runProject
+
+make :: Language -> Action -> IO ()
+make langOption actOption = execute (language langOption) (action actOption)
+
+execute :: ProjectFunc -> ActionFunc -> IO ()
+execute languageFunc actionFunc = do
+    languageFunc (readProject >>= actionFunc) >>= \case
+        (Left err) -> Logging.writeStderr (Text.pack err)
+        (Right _ ) -> return ()
+
+-----------------------------------------------------------------------------------
+{- Singletons - Projects Without A Project File -}
+
+type ProjectTransform = ProjectData -> ProjectData
+
+makeSingle :: [ProjectTransform] -> Language -> Action -> IO ()
+makeSingle transforms langOption actOption = do
+    let projectData = foldr ($) projectDataEmpty transforms
+    language langOption (action actOption projectData) >>= \case
+        (Left err) -> Logging.writeStderr (Text.pack err)
+        (Right _ ) -> return ()
