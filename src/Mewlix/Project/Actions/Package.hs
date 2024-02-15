99ecb682437a770aa3b2021aae538517c0feb538
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-15 12:38:57 -0300

    feat: packageProject function now properly packages the contents of the output folder, using conduits for streaming

--- src/Mewlix/Project/Actions/Package.hs
+++ src/Mewlix/Project/Actions/Package.hs
@@ -4,24 +4,44 @@ module Mewlix.Project.Actions.Package
 ( packageProject
 ) where
 
-import Mewlix.Project.Data.Types (ProjectData)
+import Mewlix.Project.Data.Types (ProjectData(..))
 import Mewlix.Project.Maker (ProjectMaker)
 import Mewlix.Project.Actions.Build (buildProject)
 import Mewlix.Project.Folder (outputFolder)
 import Mewlix.Project.Log (projectLog)
 import System.Directory (doesDirectoryExist)
 import Codec.Archive.Zip
-    ( createArchive
-    , packDirRecur
-    , CompressionMethod(Deflate)
+    ( ZipArchive
+    , sinkEntry
     , mkEntrySelector
+    , CompressionMethod(Deflate)
+    , createArchive
     )
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad (unless)
+import Conduit (sourceFile, sourceDirectoryDeep, runConduitRes, sinkList, (.|))
+import System.FilePath (makeRelative, makeValid, replaceExtension)
+import qualified Data.Text as Text
 
-createPackage :: (MonadIO m) => FilePath -> m ()
-createPackage targetPath = liftIO (createArchive targetPath packDirectory)
-    where packDirectory = packDirRecur Deflate mkEntrySelector outputFolder
+packageFolder :: FilePath -> ZipArchive ()
+packageFolder folder = do
+    let copy :: FilePath -> ZipArchive ()
+        copy filepath = do
+            let name = makeRelative folder filepath
+            selector <- mkEntrySelector name
+            sinkEntry Deflate (sourceFile filepath) selector
+
+    filepaths <- liftIO $ runConduitRes
+         $ sourceDirectoryDeep True folder
+        .| sinkList
+    mapM_ copy filepaths
+
+createPackage :: (MonadIO m) => ProjectData -> m ()
+createPackage projectData = do
+    let name = Text.unpack (projectName projectData)
+    let packageName = makeValid name `replaceExtension` ".zip"
+
+    liftIO $ createArchive packageName (packageFolder outputFolder)
 
 packageProject :: ProjectData -> ProjectMaker ()
 packageProject projectData = do
@@ -30,4 +50,4 @@ packageProject projectData = do
         buildProject projectData
 
     projectLog projectData "Packaging project..."
-    createPackage outputFolder
+    createPackage projectData
