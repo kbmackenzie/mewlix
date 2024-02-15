ecab436e05f734db3bc1827c18c4ace91ff721c5
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-15 14:13:51 -0300

    feat: createReadme function now listens to the NoReadMe flag and doesn't generate a new README.md when that flag is active

--- src/Mewlix/Project/Templates/ReadMe.hs
+++ src/Mewlix/Project/Templates/ReadMe.hs
@@ -4,22 +4,21 @@ module Mewlix.Project.Templates.ReadMe
 ( createReadme
 ) where
 
-import Mewlix.Project.Data.Types (ProjectData(..))
+import Mewlix.Project.Data.Types (ProjectData(..), ProjectFlag(..))
 import Mewlix.Project.Folder (outputFolder)
 import Mewlix.Utils.FileIO (writeFileT)
+import qualified Data.Set as Set
 import qualified Data.Text as Text
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import System.FilePath ((</>))
-import System.Directory (doesFileExist)
 import Control.Monad (unless)
 
 createReadme :: (MonadIO m) => ProjectData -> m ()
 createReadme projectData = do
-    let path = outputFolder </> "README.md"
-    let contents = Text.unlines
-            [ "# " <> projectName projectData 
-            , projectDescription projectData  ]
-
-    exists <- liftIO (doesFileExist path)
-    unless exists $
+    let noReadMe = Set.member NoReadMe (projectFlags projectData)
+    unless noReadMe $ do
+        let path = outputFolder </> "README.md"
+        let contents = Text.unlines
+                [ "# " <> projectName projectData 
+                , projectDescription projectData  ]
         liftIO (writeFileT path contents)
