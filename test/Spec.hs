5ca2ac6019b9c9cffeae56be0f054d2b3e673c29
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-18 00:14:45 -0300

    test: Added better error logging on test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,14 +7,24 @@ import Mewlix.Compiler.Transpiler (emptyContext)
 import Mewlix.Utils.FileIO (readFileT)
 import System.Environment (getArgs)
 import qualified Data.Text.IO as TextIO
+import System.IO (stderr, hPutStrLn)
+import Control.Exception (throwIO, Exception(..))
+
+newtype ParseException = ParseException String
+    deriving (Show)
+
+instance Exception ParseException where
 
 compileFile :: FilePath -> IO Text
 compileFile path = do
     contents <- readFileT path >>= \case
-        (Left e)    -> undefined
+        (Left e)    -> throwIO e
         (Right a)   -> return a
     case compileJS emptyContext path contents of
-        (Left e)    -> undefined
+        (Left e)    -> do
+            hPutStrLn stderr e
+            (throwIO . ParseException . concat)
+                    [ "Couldn't parse file \"", path, "\"!" ]
         (Right a)   -> return a
 
 main :: IO ()
