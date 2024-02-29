22a027b63c0a31613d513150668a75d927311e38
KBMackenzie <kelly.a.betty@gmail.com>
2024-02-28 22:35:28 -0300

    feat: Mewlix.Project.Log uses Mewlix.Logger for logging messages and errors now

--- src/Mewlix/Project/Log.hs
+++ src/Mewlix/Project/Log.hs
@@ -6,26 +6,21 @@ module Mewlix.Project.Log
 ) where
 
 import Mewlix.Project.Data.Types (ProjectData(..), ProjectFlag(..))
+import Mewlix.Logger (LogType(..), logger);
 import Data.Text (Text)
-import qualified Data.Text.IO as TextIO
-import Control.Monad.IO.Class (MonadIO, liftIO)
-import System.IO (Handle, stdout, stderr)
-import Control.Monad (unless)
 import qualified Data.Set as Set
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad (unless)
 
 projectMessage :: ProjectData -> Text -> Text
-projectMessage projectData message = mconcat [ "[" , projectName projectData , "] " , message ]
-
-loggerBase :: (MonadIO m) => Handle -> ProjectData -> Text -> m ()
-loggerBase handle projectData message = do
-    let str = projectMessage projectData message
-    liftIO (TextIO.hPutStr handle str)
+projectMessage projectData message = mconcat
+    [ "[", projectName projectData, "] ", message ]
 
 projectLog :: (MonadIO m) => ProjectData -> Text -> m ()
 projectLog projectData message = do
     let quiet = Set.member Quiet (projectFlags projectData)
     unless quiet $
-        loggerBase stdout projectData message
+        logger Info (projectMessage projectData message)
 
 projectLogError :: (MonadIO m) => ProjectData -> Text -> m ()
-projectLogError = loggerBase stderr
+projectLogError = (logger Error .) . projectMessage
