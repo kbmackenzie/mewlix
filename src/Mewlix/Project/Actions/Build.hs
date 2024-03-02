99a708df1a2e77dbd4a499c1318f0c1d01a5f252
KBMackenzie <kelly.a.betty@gmail.com>
2024-03-02 02:51:01 -0300

    feat: When generating script-list.json, ensure all script paths are relative to outputFolder

--- src/Mewlix/Project/Actions/Build.hs
+++ src/Mewlix/Project/Actions/Build.hs
@@ -5,14 +5,14 @@ module Mewlix.Project.Actions.Build
 ) where
 
 import Mewlix.Project.Maker (ProjectMaker)
-import Mewlix.Project.Folder (coreFolder)
 import Mewlix.Project.Data.Types (ProjectData(..))
+import Mewlix.Project.Folder (coreFolder, outputFolder)
 import Mewlix.Project.Modules.Compile (compileModules)
 import Mewlix.Project.Templates.Create (createFromTemplate)
 import Mewlix.Project.Templates.ReadMe (createReadme)
 import Mewlix.Project.Assets.Copy (copyAssets)
 import Mewlix.Project.Log (projectLog)
-import System.FilePath ((</>))
+import System.FilePath ((</>), makeRelative)
 import Mewlix.Utils.FileIO (writeFileBL)
 import Data.Aeson (object, (.=), encode)
 
@@ -35,7 +35,9 @@ scriptList :: ProjectData -> [FilePath] -> ProjectMaker ()
 scriptList projectData paths = do
     let targetPath = coreFolder </> "script-list.json"
     let entrypoint = projectEntrypoint projectData
+    let relativePaths = map (makeRelative outputFolder) paths
+
     let script = object
             [ "entrypoint" .= entrypoint
-            , "scripts"    .= paths         ]
+            , "scripts"    .= relativePaths ]
     writeFileBL targetPath (encode script)
