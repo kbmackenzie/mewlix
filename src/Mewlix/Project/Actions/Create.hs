27419cae8f13f0201be376c25106f9249ed38103
KBMackenzie <kelly.a.betty@gmail.com>
2024-03-02 04:24:04 -0300

    feat: Project's 'new' action will now log a nice message when creating a new project

--- src/Mewlix/Project/Actions/Create.hs
+++ src/Mewlix/Project/Actions/Create.hs
@@ -5,8 +5,10 @@ module Mewlix.Project.Actions.Create
 ) where
 
 import Mewlix.Project.Data.Types (ProjectData(..), projectFieldOrder)
+import Mewlix.Project.Log (projectLog)
 import Mewlix.Utils.Yaml (prettyYaml)
 import Mewlix.Utils.FileIO (writeFileB)
+import Mewlix.Utils.Show (showT)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import qualified Data.Text as Text
 import System.FilePath (replaceExtension, makeValid)
@@ -14,6 +16,9 @@ import System.Directory (createDirectoryIfMissing)
 
 createProject :: (MonadIO m) => ProjectData -> m ()
 createProject projectData = do
+    -- Log project name + a nice message
+    projectLog projectData "Creating project file..."
+
     -- Create project file:
     let name = (makeValid . Text.unpack . projectName) projectData
     let path = replaceExtension ("./" <> name) "mewlix"
