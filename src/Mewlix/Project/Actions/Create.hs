41e64eb5e7b86e1997ff80a0589d178a151e2b56
KBMackenzie <kelly.a.betty@gmail.com>
2024-03-02 04:03:02 -0300

    feat: Project's 'new' action now creates a 'src/' directory too if it doesn't exist

--- src/Mewlix/Project/Actions/Create.hs
+++ src/Mewlix/Project/Actions/Create.hs
@@ -7,13 +7,18 @@ module Mewlix.Project.Actions.Create
 import Mewlix.Project.Data.Types (ProjectData(..), projectFieldOrder)
 import Mewlix.Utils.Yaml (prettyYaml)
 import Mewlix.Utils.FileIO (writeFileB)
-import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.IO.Class (MonadIO, liftIO)
 import qualified Data.Text as Text
 import System.FilePath (replaceExtension, makeValid)
+import System.Directory (createDirectoryIfMissing)
 
 createProject :: (MonadIO m) => ProjectData -> m ()
 createProject projectData = do
+    -- Create project file:
     let name = (makeValid . Text.unpack . projectName) projectData
     let path = replaceExtension ("./" <> name) "mewlix"
     let contents = prettyYaml projectFieldOrder projectData
     writeFileB path contents
+
+    -- Create 'src' directory:
+    liftIO (createDirectoryIfMissing False "./src")
