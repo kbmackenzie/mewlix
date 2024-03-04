8634c30857014b1f6ac884503735e16506639253
KBMackenzie <kelly.a.betty@gmail.com>
2024-03-04 16:05:04 -0300

    feat: Add 'make' util + re-export Action data type in Mewlix module

--- src/Mewlix.hs
+++ src/Mewlix.hs
@@ -1,5 +1,11 @@
 module Mewlix
 ( run
+, make
+, Action(..)
 ) where
 
 import Mewlix.CLI.Main (run)
+import Mewlix.Project.Make (Action(..), make', Language(..))
+
+make :: Action -> IO ()
+make = make' Javascript
