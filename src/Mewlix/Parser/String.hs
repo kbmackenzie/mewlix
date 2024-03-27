927b8abbb7664c223631328b30c75e541dbc448a
KBMackenzie <kelly.a.betty@gmail.com>
2024-03-27 01:24:15 -0300

    feat: Added string interpolation parser

---
+++ src/Mewlix/Parser/String.hs
@@ -0,0 +1,107 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Mewlix.Parser.String
+( parseString
+, parseStringM
+, parseYarnString
+) where
+
+import Mewlix.Abstract.AST
+    ( Primitive(..)
+    , BinaryOp(..)
+    , Expression(..)
+    )
+import Mewlix.Parser.Utils
+    ( Parser
+    , lexeme
+    , brackets
+    )
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Text.Megaparsec ((<?>))
+import qualified Text.Megaparsec as Mega
+import qualified Text.Megaparsec.Char as MChar
+import Control.Monad (void)
+
+{- Escape sequences: -}
+----------------------------------------------------------------
+escapeChar :: Char -> Char
+escapeChar c = case c of
+    'n'     -> '\n'
+    't'     -> '\t'
+    'b'     -> '\b'
+    'r'     -> '\r'
+    'f'     -> '\f'
+    'v'     -> '\v'
+    other   -> other
+
+{- Single-line strings: -}
+----------------------------------------------------------------
+stringChar :: Parser Char
+stringChar = Mega.choice
+    [ MChar.char '\\' >> fmap escapeChar Mega.anySingle
+    , MChar.newline >> fail "Linebreak in string!"
+    , Mega.satisfy (/= '"')                             ]
+
+parseString :: Parser Text
+parseString = (<?> "string") $ do
+    let quotation :: Parser ()
+        quotation = (void . MChar.char) '"' <?> "quotation mark"
+
+    quotation
+    text <- Text.pack <$> Mega.many stringChar
+    lexeme quotation
+    return text
+
+{- Multi-line strings: -}
+----------------------------------------------------------------
+stringCharM :: Parser Char
+stringCharM = Mega.choice
+    [ MChar.char '\\' >> fmap escapeChar Mega.anySingle
+    , Mega.satisfy (/= '"')                             ]
+
+parseStringM :: Parser Text
+parseStringM = (<?> "multiline string") $ do
+    let quotations :: Parser ()
+        quotations = (void . MChar.string) "\"\"\"" <?> "triple quotes"
+
+    quotations
+    text <- Text.pack <$> Mega.many stringCharM
+    lexeme quotations
+    return text
+
+{- String interpolation ('yarn string'): -}
+----------------------------------------------------------------
+stringToExpr :: Text -> Expression
+stringToExpr = PrimitiveExpr . MewlixString
+
+interpolate :: [Expression] -> Expression
+interpolate [] = stringToExpr ""
+interpolate xs = foldr1 (BinaryOperation StringConcat) xs
+
+stringCharI :: Parser Char
+stringCharI = Mega.choice
+    [ MChar.char '\\' >> fmap escapeChar Mega.anySingle
+    , MChar.newline >> fail "Linebreak in string!"
+    , Mega.satisfy (\x -> x /= '"' && x /= '[')        ]
+
+stringBite :: Parser Text
+stringBite = Text.pack <$> Mega.some stringCharI
+
+parseYarnString :: Parser Expression -> Parser Expression
+parseYarnString expression = do
+    let quotation :: Parser ()
+        quotation = (void . MChar.char) '"' <?> "quotation mark"
+
+    let yarnMeow :: Parser ()
+        yarnMeow = void (MChar.string' ":3\"" <?> "yarn string")
+
+    let stringPiece :: Parser Expression
+        stringPiece = Mega.choice
+            [ stringToExpr <$> stringBite
+            , brackets expression         ]
+
+    yarnMeow
+    pieces <- Mega.many stringPiece 
+    quotation
+    return (interpolate pieces)
