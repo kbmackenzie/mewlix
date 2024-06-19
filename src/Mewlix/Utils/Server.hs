5a386df49fde3ef10358dcd9d362cd1da0908f1b
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-19 17:52:51 -0300

    fix(utils): When sending server response, encode string as UTF8

--- src/Mewlix/Utils/Server.hs
+++ src/Mewlix/Utils/Server.hs
@@ -9,6 +9,7 @@ import qualified Network.Wai.Handler.Warp as Warp
 import Network.Wai.Middleware.Static (staticPolicy, addBase)
 import Network.Wai (Application, pathInfo, requestMethod, responseFile, responseLBS)
 import Network.HTTP.Types (methodGet, status200, status404, Header)
+import Data.ByteString.Lazy.UTF8 (fromString)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import System.FilePath ((</>))
 
@@ -28,7 +29,8 @@ serveRoot base req respond
         respond $ responseFile status200 headers index Nothing
     -- Do not handle any other form of request.
     | otherwise = do
-        respond $ responseLBS status404 [html] "<h1>404: not found 🐱</h1>"
+        let message = fromString "<h1>404: not found 🐱</h1>"
+        respond $ responseLBS status404 [html] message
 
 serve :: (MonadIO m) => FilePath -> Port -> m ()
 serve base port = do
