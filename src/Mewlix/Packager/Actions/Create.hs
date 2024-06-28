01f715652dd365f46b4fac3cbb8c25771417a480
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-28 06:32:02 -0300

    feat(packager): When creating new project, check for existing one

--- src/Mewlix/Packager/Actions/Create.hs
+++ src/Mewlix/Packager/Actions/Create.hs
@@ -4,24 +4,30 @@ module Mewlix.Packager.Actions.Create
 ( createProject
 ) where
 
-import Mewlix.Packager.Data.Types (ProjectData(..), projectFieldOrder, projectSourceFilesL)
+import Mewlix.Packager.Maker (PackageMaker, throwError, liftIO)
 import Mewlix.Packager.Log (projectLog)
+import Mewlix.Packager.Data.Types (ProjectData(..), projectFieldOrder, projectSourceFilesL)
 import Mewlix.Packager.Folder (projectFile, outputFolder)
 import Mewlix.Utils.Yaml (prettyYaml)
 import Mewlix.Utils.FileIO (writeBytes)
-import Control.Monad.IO.Class (MonadIO, liftIO)
-import System.Directory (createDirectoryIfMissing)
+import System.Directory (doesFileExist, createDirectoryIfMissing)
 import Lens.Micro.Platform ((%~))
 import System.FilePath (addTrailingPathSeparator)
 import qualified Data.ByteString.Char8 as ByteString
+import Control.Monad (when)
 
 includeSrc :: ProjectData -> ProjectData
 includeSrc = projectSourceFilesL %~ ("src/" :)
 
-createProject :: (MonadIO m) => ProjectData -> m ()
+createProject :: ProjectData -> PackageMaker ()
 createProject = (. includeSrc) $ \projectData -> do
     -- Log project name + a nice message
     projectLog projectData "Creating project file..."
+
+    -- Check for existing project
+    exists <- liftIO (doesFileExist projectFile)
+    when exists $ do
+        throwError "Cannot create project: There's already a project in this directory!"
 
     -- Create project file:
     let contents = prettyYaml projectFieldOrder projectData
