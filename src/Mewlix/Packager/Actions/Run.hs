c17f768e3fe6f57828aac14f66485cfb6cf0be48
kbmackenzie <kelly.a.betty@gmail.com>
2024-09-01 17:47:10 -0300

    feat(packager): When running, rebuild when asked

--- src/Mewlix/Packager/Actions/Run.hs
+++ src/Mewlix/Packager/Actions/Run.hs
@@ -4,17 +4,20 @@ module Mewlix.Packager.Actions.Run
 
 import Mewlix.Packager.Maker (PackageMaker, liftIO)
 import Mewlix.Packager.Folder (outputFolder)
-import Mewlix.Packager.Data.Types (ProjectData(..), ProjectMode(..))
+import Mewlix.Packager.Data.Types (ProjectData(..), ProjectMode(..), ProjectFlag(..))
 import Mewlix.Packager.Actions.Build (buildProject)
 import Mewlix.Packager.Actions.Server (runServer)
 import Mewlix.Packager.Actions.Node (runNode)
-import Control.Monad (unless)
+import Control.Monad (when)
 import System.Directory (doesDirectoryExist)
+import qualified Data.Set as Set
 
 runProject :: ProjectData -> PackageMaker ()
 runProject projectData = do
+    let rebuild = Set.member Rebuild (projectFlags projectData)
     exists <- liftIO (doesDirectoryExist outputFolder)
-    unless exists $
+
+    when (not exists || rebuild) $
         buildProject projectData
 
     case projectMode projectData of
