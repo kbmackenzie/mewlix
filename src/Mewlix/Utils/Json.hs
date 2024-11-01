9449ad29f6060cc63fda35114a94a50964b5c292
kbmackenzie <kelly.a.betty@gmail.com>
2024-11-01 03:59:30 -0300

    feat(utils): Add JSON serialization util

--- src/Mewlix/Utils/Json.hs
+++ src/Mewlix/Utils/Json.hs
@@ -1,9 +1,13 @@
 module Mewlix.Utils.Json
 ( parseJson
+, serializeJson
 ) where
 
-import Data.Aeson (eitherDecodeStrict, FromJSON)
-import Data.ByteString (ByteString)
+import Data.Aeson (eitherDecodeStrict, encode, FromJSON, ToJSON)
+import Data.ByteString (ByteString, toStrict)
 
 parseJson :: (FromJSON a) => ByteString -> Either String a
 parseJson = eitherDecodeStrict
+
+serializeJson :: (ToJSON a) => a -> ByteString
+serializeJson = toStrict . encode
