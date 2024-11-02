5ae8cb2fdea7a2bf59628d3b4b2800768b7e0d48
kbmackenzie <kelly.a.betty@gmail.com>
2024-11-02 04:32:35 -0300

    test: Add expression parser tests + do a little clean-up

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Mewlix.Abstract.AST
+import Mewlix.Abstract.Key
 import Mewlix.Compiler
+import Mewlix.Parser
 import Data.Text (Text)
 import Test.Hspec
     ( Spec
@@ -15,26 +18,26 @@ import Data.Either (isRight)
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as TextEncoding
 import qualified Data.ByteString as ByteString
-import Prelude hiding (readFile)
+import Prelude hiding (readFile, negate, concat, and, or)
 import System.FilePath ((</>))
 import System.FilePattern.Directory (getDirectoryFiles)
 
 readFile :: FilePath -> IO Text
 readFile = fmap TextEncoding.decodeUtf8 . ByteString.readFile
 
-snippets :: [(Text, Text)] -> Spec
-snippets = describe "parse mewlix snippets" . do
-    let snippet :: (Text, Text) -> Spec
-        snippet (input, expected) = do
+expressions :: [(Text, Expression)] -> Spec
+expressions = describe "parse mewlix expressions" . do
+    let expression :: (Text, Expression) -> Spec
+        expression (input, expected) = do
             let preview = if Text.length input >= 50
                 then Text.take 50 input `Text.append` "..."
                 else input
             let description = "should parse the expression " ++ show preview
-            let compile = compileJS emptyContext "expression"
+            let parse = parseExpr "expression"
 
             it description $
-                compile input `shouldBe` Right expected
-    mapM_ snippet
+                parse input `shouldBe` Right expected
+    mapM_ expression
 
 scripts :: [FilePath] -> Spec
 scripts = do
@@ -53,8 +56,34 @@ getYarnBalls = do
     let directory = "." </> "test" </> "YarnBalls"
     map (directory </>) <$> getDirectoryFiles directory ["*.mews"]
 
+basicExpressions :: [(Text, Expression)]
+basicExpressions = 
+    [ ("1 + 2"              , add (number 1) (number 2)                                    )
+    , ("-1 + 2"             , add (negate (number 1)) (number 2)                           )
+    , ("1 + 2 * 3"          , add (number 1) (multiply (number 2) (number 3))              )
+    , ("(1 + 2) * 3"        , multiply (add (number 1) (number 2)) (number 3)              )
+    , ("a .. b .. c"        , concat (concat (variable "a") (variable "b")) (variable "c") )
+    , ("a..b..c"            , concat (concat (variable "a") (variable "b")) (variable "c") )
+    , ("a push b"           , push (variable "a") (variable "b")                           )
+    , ("a push b push c"    , push (push (variable "a") (variable "b")) (variable "c")     )
+    , ("paw at a push b"    , push (peek (variable "a")) (variable "b")                    )
+    , ("a and b or c"       , or (and (variable "a") (variable "b")) (variable "c")        )
+    , ("a or b and c"       , or (variable "a") (and (variable "b") (variable "c"))        )
+    ]
+    where number   = PrimitiveExpr . MewlixInt
+          add      = BinaryOperation Addition
+          multiply = BinaryOperation Multiplication
+          negate   = UnaryOperation Minus
+          variable = Identifier . Key
+          concat   = BinaryOperation StringConcat
+          push     = BinaryOperation ShelfPush
+          peek     = UnaryOperation ShelfPeek
+          and      = BooleanAnd
+          or       = BooleanOr
+
 main :: IO ()
 main = do
     yarnballs <- getYarnBalls
     hspec $ do
-        describe "mewlix parser & compiler" (scripts yarnballs)
+        describe "basic yarnballs"   (scripts yarnballs)
+        describe "basic expressions" (expressions basicExpressions)
