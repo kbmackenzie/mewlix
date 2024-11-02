797de3b34026d9a2b7dfa90de4a7dd32a4e2153f
kbmackenzie <kelly.a.betty@gmail.com>
2024-11-02 06:12:34 -0300

    fix(packager): Fix asset logic + clean it up

--- src/Mewlix/Packager/Build/Assets.hs
+++ src/Mewlix/Packager/Build/Assets.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Mewlix.Packager.Build.Assets
 ( copyAssets
 ) where
@@ -5,28 +7,32 @@ module Mewlix.Packager.Build.Assets
 import Mewlix.Packager.Type (Packager)
 import Mewlix.Packager.Config (ProjectConfig(..))
 import Mewlix.Packager.Environment (buildFolder)
+import Mewlix.Packager.Log (projectLog)
 import Mewlix.Utils.IO (safelyRun, copyFileSafe, createDirectory, compareFileMods)
+import Mewlix.Utils.Show (showT)
 import System.FilePath ((</>), takeDirectory)
 import System.FilePattern.Directory (getDirectoryFiles)
 import Control.Monad (when)
 
-copyAsset :: FilePath -> Packager ()
-copyAsset inputPath = do
+copyAsset :: ProjectConfig -> FilePath -> Packager ()
+copyAsset config asset = do
     let prepareDirectory :: FilePath -> Packager ()
         prepareDirectory = createDirectory True . takeDirectory
 
-    let outputPath = buildFolder </> inputPath
-    prepareDirectory outputPath
+    let output = buildFolder </> asset
+    prepareDirectory output
 
-    shouldCopy <- maybe True (== GT) <$> compareFileMods inputPath outputPath
-    when shouldCopy (copyFileSafe inputPath outputPath)
+    shouldCopy <- maybe True (== GT) <$> compareFileMods asset output
+    when shouldCopy $ do
+        projectLog config ("Copying asset " <> showT asset)
+        copyFileSafe asset output
 
 findAssets :: ProjectConfig -> Packager [FilePath]
 findAssets config = do
-    let patterns = projectSourceFiles config
+    let patterns = projectAssets config
     safelyRun (getDirectoryFiles "." patterns) "couldn't get asset files"
 
 copyAssets :: ProjectConfig -> Packager ()
 copyAssets config = do
     assets <- findAssets config
-    mapM_ copyAsset assets
+    mapM_ (copyAsset config) assets
