c95418d8a5ce7f3808a1e8aa1d100e55fb8e9d9c
kbmackenzie <kelly.a.betty@gmail.com>
2024-12-10 15:18:20 -0300

    fix(packager): Respect 'no-browser' project flag

--- src/Mewlix/Packager/Actions/Server.hs
+++ src/Mewlix/Packager/Actions/Server.hs
@@ -6,13 +6,14 @@ module Mewlix.Packager.Actions.Server
 
 import Mewlix.Packager.Type (Packager)
 import Mewlix.Packager.Environment (buildFolder)
-import Mewlix.Packager.Config (ProjectConfig(..), Port(..))
+import Mewlix.Packager.Config (ProjectConfig(..), Port(..), ProjectFlag(..))
 import Mewlix.Packager.Log (projectLog)
 import Mewlix.Utils.Server (serve)
 import Control.Monad.IO.Class (liftIO)
-import Control.Monad (void)
+import Control.Monad (void, unless)
 import Mewlix.Utils.Show (showT)
 import Web.Browser (openBrowser)
+import qualified Data.Set as Set
 
 runServer :: ProjectConfig -> Packager ()
 runServer config = do
@@ -25,8 +26,10 @@ runServer config = do
         , (showT . getPort) port ]
 
     -- Open browser:
-    let address = concat ["http://localhost:", (show . getPort) port, "/"]
-    (liftIO . void . openBrowser) address
+    let noBrowser = Set.member NoBrowser (projectFlags config)
+    unless noBrowser . void . liftIO $ do
+        let address = concat ["http://localhost:", (show . getPort) port, "/"]
+        openBrowser address
 
     -- Run server:
     projectLog config "Press (Ctrl + C) to exit."
