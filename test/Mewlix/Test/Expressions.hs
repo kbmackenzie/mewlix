650cea65fbc586f9b8e8d37bd080bfd2844ca243
kbmackenzie <kelly.a.betty@gmail.com>
2025-01-13 05:16:14 -0300

    test: Add more expression parser tests, focusing on shelves and function calls

--- test/Mewlix/Test/Expressions.hs
+++ test/Mewlix/Test/Expressions.hs
@@ -10,6 +10,7 @@ import Mewlix.Abstract.AST
     , BinaryOp(..)
     , UnaryOp(..)
     , Params(..)
+    , Arguments(..)
     )
 import Mewlix.Abstract.Key (Key(..))
 import Mewlix.Parser (runParser, expression)
@@ -32,6 +33,7 @@ testExpressions = do
 
 expressions :: [(Text, Expression)]
 expressions = 
+    -- Basic expressions:
     [ ("1 + 2"              , add (number 1) (number 2)                                                 )
     , ("-1 + 2"             , add (negate (number 1)) (number 2)                                        )
     , ("1 + 2 * 3"          , add (number 1) (multiply (number 2) (number 3))                           )
@@ -51,19 +53,35 @@ expressions =
     , ("a == b == c"        , equal (equal (variable "a") (variable "b")) (variable "c")                )
     , ("a .. b == c"        , equal (concat (variable "a") (variable "b")) (variable "c")               )
     , ("a == b .. c"        , equal (variable "a") (concat (variable "b") (variable"c"))                )
-    , ("a()()()"            , call (call (call (variable "a")))                                         )
-    , ("a()['b']()"         , call (lookup (call (variable "a")) (str "b"))                             )
-    , ("a['b']()()"         , call (call (lookup (variable "a") (str "b")))                             )
-    , ("a()['b']['c']()()"  , call (call (lookup (lookup (call (variable "a")) (str "b")) (str "c")))   )
-    , ("a['b']()['c']()()"  , call (call (lookup (call (lookup (variable "a") (str "b"))) (str "c")))   )
-    , ("a.b()['c']"         , lookup (call (dot (variable "a") (prop "b"))) (str "c")                   )
-    , ("a['b'].c()"         , call (dot (lookup (variable "a") (str "b")) (prop "c"))                   )
+    -- Functions calls + property access:
+    , ("a()()()"            , call' (call' (call' (variable "a")))                                      )
+    , ("a()['b']()"         , call' (lookup (call' (variable "a")) (str "b"))                           )
+    , ("a['b']()()"         , call' (call' (lookup (variable "a") (str "b")))                           )
+    , ("a()['b']['c']()()"  , call' (call' (lookup (lookup (call' (variable "a")) (str "b")) (str "c"))))
+    , ("a['b']()['c']()()"  , call' (call' (lookup (call' (lookup (variable "a") (str "b"))) (str "c"))))
+    , ("a.b()['c']"         , lookup (call' (dot (variable "a") (prop "b"))) (str "c")                  )
+    , ("a['b'].c()"         , call' (dot (lookup (variable "a") (str "b")) (prop "c"))                  )
     , ("a.b.c.d"            , dot (dot (dot (variable "a") (prop "b")) (prop "c")) (prop "d")           )
-    , ("a().b.c.d"          , dot (dot (dot (call (variable "a")) (prop "b")) (prop "c")) (prop "d")    )
+    , ("a().b.c.d"          , dot (dot (dot (call' (variable "a")) (prop "b")) (prop "c")) (prop "d")   )
+    -- Lambdas:
     , ("=^oxo^= (x) -> x"   , lambda (param "x") (variable "x")                                         )
     , ("🐈 (x) -> x"        , lambda (param "x") (variable "x")                                         )
-    , ("(🐈 (x) -> x)()"    , call (lambda (param "x") (variable "x"))                                  )
+    , ("(🐈 (x) -> x)()"    , call' (lambda (param "x") (variable "x"))                                 )
+    , ("(🐈 (x) -> x)(3)"   , call (lambda (param "x") (variable "x")) (args [number 3])                )
     , ("🐈 () -> '🐈'"      , lambda mempty (str "🐈")                                                  )
+    -- Function calls:
+    , ("a(b)"               , call (variable "a") (args [variable "b"])                                 )
+    , ("a(b, c, d)"         , call (variable "a") (args [variable "b", variable "c", variable "d"])     )
+    , ("a(b)(c)"            , call (call (variable "a") (args [variable "b"])) (args [variable "c"])    )
+    , ("a(\nb,\nc,\n)"      , call (variable "a") (args [variable "b", variable "c"])                   )
+    , ("a(\nb,\nc,\n)"      , call (variable "a") (args [variable "b", variable "c"])                   )
+    -- Shelves:
+    , ("[]"                 , shelf []                                                                  )
+    , ("[\n]"               , shelf []                                                                  )
+    , ("[\n\n\n]"           , shelf []                                                                  )
+    , ("[a, b, c]"          , shelf [variable "a", variable "b", variable "c"]                          ) 
+    , ("[\na,\nb,\nc,\n]"   , shelf [variable "a", variable "b", variable "c"]                          )
+    , ("[\na,\nb,\nc,\n]"   , shelf [variable "a", variable "b", variable "c"]                          )
     ]
     where number   = PrimitiveExpr . MewlixInt
           add      = BinaryOperation Addition
@@ -79,13 +97,16 @@ expressions =
           true     = PrimitiveExpr . MewlixBool $ True
           false    = PrimitiveExpr . MewlixBool $ False
           equal    = BinaryOperation Equal
-          call     = flip FunctionCall mempty
           lookup   = LookupExpression
           dot      = DotExpression
           str      = PrimitiveExpr . MewlixString
           prop     = ObjectProperty . Key
           lambda   = LambdaExpression
           param    = Params . List.singleton . Key
+          args     = Arguments
+          call     = FunctionCall
+          call'    = flip FunctionCall mempty
+          shelf    = ShelfExpression
 
 test :: Spec
 test = testExpressions expressions
